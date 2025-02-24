819fb90bf3da0aa515243a483ede826d230206e9
Rizky Ardiansyah <ardiansr@proton.me>
2025-02-24 10:50:41 +0700

    test: add `text_node_to_html_node` function to convert text node to HTML node

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,7 @@
 import unittest
 
-from textnode import TextNode, TextType
+from htmlnode import LeafNode
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -19,3 +20,33 @@ class TestTextNode(unittest.TestCase):
                                        "https://www.boot.dev")
         self.assertEqual(repr(text_node),
                          "TextNode(Boot dot dev, link, https://www.boot.dev)")
+
+
+class TestTextNodeToHTMLNode(unittest.TestCase):
+    def test_eq(self) -> None:
+        text_node: TextNode = TextNode("A normal text", TextType.TEXT)
+        self.assertEqual(text_node_to_html_node(text_node),
+                         LeafNode(None, "A normal text"))
+
+    def test_italic_eq(self) -> None:
+        text_node: TextNode = TextNode("An italic text", TextType.ITALIC)
+        self.assertEqual(text_node_to_html_node(text_node),
+                         LeafNode("i", "An italic text"))
+
+    def test_link_eq(self) -> None:
+        text_node: TextNode = TextNode("Click me!", TextType.LINK,
+                                       "https://www.boot.dev")
+        self.assertEqual(
+                text_node_to_html_node(text_node),
+                LeafNode("a", "Click me!", {"href": "https://www.boot.dev"}))
+
+    def test_image_eq(self) -> None:
+        text_node: TextNode = TextNode(
+                "Beautiful daisy flowers",
+                TextType.IMAGE,
+                "https://unsplash.com/photos/a-bunch-of-white-and-yellow-flowers-in-a-field-y-HLtKtZLqg")
+        self.assertEqual(
+                text_node_to_html_node(text_node),
+                LeafNode("img", "",
+                         {"src": "https://unsplash.com/photos/a-bunch-of-white-and-yellow-flowers-in-a-field-y-HLtKtZLqg",
+                          "alt": "Beautiful daisy flowers"}))
--- src/textnode.py
+++ src/textnode.py
@@ -2,6 +2,8 @@ from enum import Enum
 from types import NotImplementedType
 from typing import Optional
 
+from htmlnode import HTMLNode, LeafNode
+
 
 class TextType(Enum):
     TEXT = "normal"
@@ -20,7 +22,7 @@ class TextNode:
         self.text_type = text_type
         self.url = url
 
-    def __eq__(self, other: object, /) -> bool | NotImplementedType:
+    def __eq__(self, other: object) -> bool | NotImplementedType:
         if not isinstance(other, TextNode):
             return NotImplemented
         return (other.text == self.text and
@@ -29,3 +31,20 @@ class TextNode:
 
     def __repr__(self) -> str:
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+
+def text_node_to_html_node(text_node: TextNode) -> HTMLNode:
+    match text_node.text_type:
+        case TextType.TEXT:
+            return LeafNode(None, text_node.text)
+        case TextType.BOLD:
+            return LeafNode("b", text_node.text)
+        case TextType.ITALIC:
+            return LeafNode("i", text_node.text)
+        case TextType.CODE:
+            return LeafNode("code", text_node.text)
+        case TextType.LINK:
+            return LeafNode("a", text_node.text, {"href": text_node.url})
+        case TextType.IMAGE:
+            return LeafNode("img", "", {"src": text_node.url,
+                                        "alt": text_node.text})
