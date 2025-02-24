25fa713478f6a0816463c0440b7c26845fea19b2
Rizky Ardiansyah <ardiansr@proton.me>
2025-02-24 16:21:07 +0700

    refactor: use `continue` instead of `else:`

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -8,19 +8,18 @@ def split_nodes_delimiter(
     for node in old_nodes:
         if node.text_type != TextType.TEXT:
             new_nodes.append(node)
-        else:
-            text: str = node.text
-            while delimiter in text:
-                dlen: int = len(delimiter)
-                start: int = text.find(delimiter)
-                end: int = text.find(delimiter, start + dlen)
-                if end == -1:
-                    raise ValueError(
-                            f"Missing second '{delimiter}' delimiter!")
-                if len(text[:start]) > 0:
-                    new_nodes.append(TextNode(text[:start], TextType.TEXT))
-                new_nodes.append(TextNode(text[start + dlen:end], text_type))
-                text = text[end + dlen:]
-            if len(text) > 0:
-                new_nodes.append(TextNode(text, TextType.TEXT))
+            continue
+        text: str = node.text
+        while delimiter in text:
+            dlen: int = len(delimiter)
+            start: int = text.find(delimiter)
+            end: int = text.find(delimiter, start + dlen)
+            if end == -1:
+                raise ValueError(f"Missing second '{delimiter}' delimiter!")
+            if len(text[:start]) > 0:
+                new_nodes.append(TextNode(text[:start], TextType.TEXT))
+            new_nodes.append(TextNode(text[start + dlen:end], text_type))
+            text = text[end + dlen:]
+        if len(text) > 0:
+            new_nodes.append(TextNode(text, TextType.TEXT))
     return new_nodes
